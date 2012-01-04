0fcb57a8acb01a481a2021c2874f1d27954fd531
Artūrs Gailītis <arturs.gailitis@nic.lv>
2012-01-04 15:39:23 +0200

    DbUtilFactory: support for Postgresql

--- lib/Phactory/DbUtilFactory.php
+++ lib/Phactory/DbUtilFactory.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once('DbUtil/MysqlUtil.php');
-require_once('DbUtil/SqliteUtil.php');
+require_once('DbUtil/Abstract.php');
 
 class Phactory_DbUtilFactory {
 	
@@ -13,10 +12,16 @@ class Phactory_DbUtilFactory {
 		switch ($db_type)
 		{
 			case 'mysql':
+                require_once('DbUtil/MysqlUtil.php');
 				return new Phactory_DbUtil_MysqlUtil();
 				break;
 			case 'sqlite':
+                require_once('DbUtil/SqliteUtil.php');
 				return new Phactory_DbUtil_SqliteUtil();
+                break;
+            case 'pgsql':
+                require_once('DbUtil/PgsqlUtil.php');
+                return new Phactory_DbUtil_PgsqlUtil();
 				break;
 			default:
 				throw new Exception("DB type '$db_type' not found");
