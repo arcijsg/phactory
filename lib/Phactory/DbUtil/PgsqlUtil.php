b34e59edd2ccae449eb359db7960017a80b8006e
Artūrs Gailītis <arturs.gailitis@nic.lv>
2012-01-06 13:12:48 +0200

    pgsqlUtil: lowercased columns for reading table meta-structure

--- lib/Phactory/DbUtil/PgsqlUtil.php
+++ lib/Phactory/DbUtil/PgsqlUtil.php
@@ -8,27 +8,26 @@ class Phactory_DbUtil_PgsqlUtil extends Phactory_DbUtil_Abstract
         $this->_quoteChar = '"';
     }
 
-    public function getPrimaryKey($table) {
-        $sql =
-            'SELECT '.
-              'pg_attribute.attname as Column_name '.
-            'FROM pg_index, pg_class, pg_attribute '.
-            'WHERE '.
-              'pg_class.oid = \''.$table.'\'::regclass AND '.
-              'indrelid = pg_class.oid AND '.
-              'pg_attribute.attrelid = pg_class.oid AND '.
-              'pg_attribute.attnum = ANY(pg_index.indkey) '.
-              'AND indisprimary ';
+    public function getPrimaryKey($table)
+    {
+        $sql = "SELECT pg_attribute.attname as column_name
+                  FROM pg_index, pg_class, pg_attribute
+                 WHERE
+                       pg_class.oid = '".$table."'::regclass
+                   AND indrelid = pg_class.oid
+                   AND pg_attribute.attrelid = pg_class.oid
+                   AND pg_attribute.attnum = ANY(pg_index.indkey)
+                   AND indisprimary";
         $stmt = $this->_pdo->query($sql);
 
         $result = $stmt->fetch();
-        return $result['Column_name'];
+        return $result['column_name'];
     }
 
     public function getColumns($table) {
-        $stmt = $this->_pdo->query('SELECT column_name '.
-                                       'FROM INFORMATION_SCHEMA.COLUMNS '.
-                                       'WHERE table_name = "'.$table.'"');
+        $stmt = $this->_pdo->query("SELECT column_name
+                                      FROM INFORMATION_SCHEMA.COLUMNS
+                                     WHERE table_name = '".$table."'");
         $columns = array();
         while($row = $stmt->fetch()) {
             $columns[] = $row['column_name'];
